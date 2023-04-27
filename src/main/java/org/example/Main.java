bc88fef20adfc859f7728f4b0fbe48f27273e8d9
ljihyun99 <Ji.Lee@campus.lmu.de>
2023-04-27 16:32:18 +0200

    added an array of pizzas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
+        Pizza[] callAPizzaList = new Pizza[3];
+        String[] toppingsForPeperoniPizza = {"Peperoni", "Olive"};
+        callAPizzaList[0] = new Pizza("Peperoni", "thick bread", "tomato sauce", false, toppingsForPeperoniPizza); //peperoni
+        String[] toppingsForHawaiiPizza = {"Pineapple", "Olive", "tomatos"};
+        callAPizzaList[1] = new Pizza("Hawaii", "thin bread", "tomato sauce", true, toppingsForHawaiiPizza); //hawaii
+        callAPizzaList[2] = new Pizza("margehrita", "thin bread", "tomato sauce", true); //margherita
+
+        System.out.println(callAPizzaList[0]);
 
         //peperoni pizza
-        String[] toppingsForPeperoniPizza = {"Peperoni", "Olive"};
-        Pizza peperroni_pizza = new Pizza("Peperoni", "thick bread", "tomato sauce", true, toppingsForPeperoniPizza);
-        peperroni_pizza.bake();
-        peperroni_pizza.serve();
-        System.out.println(peperroni_pizza.toString());
+//        peperroni_pizza.bake();
+//        peperroni_pizza.serve();
+//        System.out.println(peperroni_pizza);
 
         //hawaii pizza
-        String[] toppingsForHawaiiPizza = {"Pineapple", "Olive", "tomatos"};
-        Pizza hawaii_Pizza = new Pizza("Hawaii", "thin bread", "tomato sauce", true, toppingsForHawaiiPizza);
-        hawaii_Pizza.bake();
-        hawaii_Pizza.serve();
-        System.out.println(hawaii_Pizza.toString());
+//        hawaii_Pizza.bake();
+//        hawaii_Pizza.serve();
+//        System.out.println(hawaii_Pizza.toString());
 
         Pizza margherita_Pizza = new Pizza("margehrita", "thin bread", "tomato sauce", true);
-        margherita_Pizza.bake();
-        margherita_Pizza.serve();
-        System.out.println(margherita_Pizza.toString());
+//        margherita_Pizza.bake();
+//        margherita_Pizza.serve();
+//        System.out.println(margherita_Pizza.toString());
 
 
     }
